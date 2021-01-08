789295918efef7169bc10cde68eaff478f1b0191
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-08 19:33:36 +0200

    Added Repository and UoW implementations

---
+++ HumbleBob/HumbleBob.Dal/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using HumbleBob.Dal.Contexts;
+using HumbleBob.Dal.Entities;
+using HumbleBob.Dal.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HumbleBob.Dal.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(HumbleBobContext context)
+        {
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public void Create(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.SingleOrDefaultAsync(t => t.Id == id);
+        }
+
+        public void RecoverSoftDeleted(TEntity entity)
+        {
+            _dbSet.Find(entity.Id).IsDeleted = false;
+        }
+
+        public void SoftDelete(TEntity entity)
+        {
+            _dbSet.Find(entity.Id).IsDeleted = true;
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
