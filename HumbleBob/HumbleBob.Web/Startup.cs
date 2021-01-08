3b6e92bd922d260aaca9f3189db93334ab0b7825
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-08 20:08:31 +0200

    Added InitialCreate migration

--- HumbleBob/HumbleBob.Web/Startup.cs
+++ HumbleBob/HumbleBob.Web/Startup.cs
@@ -1,24 +1,29 @@
+using HumbleBob.Dal.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HumbleBob.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private const string DbConnectionStringName = "HumbleBobDb";
+
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDal(Configuration, DbConnectionStringName);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
