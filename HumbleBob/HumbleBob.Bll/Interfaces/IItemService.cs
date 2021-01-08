935dd2f65d0e3ddf5bc8fc70d14eeb348b0b63c5
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-08 22:51:06 +0200

    Added ItemService interface

---
+++ HumbleBob/HumbleBob.Bll/Interfaces/IItemService.cs
@@ -0,0 +1,22 @@
+﻿using HumbleBob.Bll.Dto;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HumbleBob.Bll.Interfaces
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<ItemDto>> GetAllAsync();
+
+        Task<ItemDto> GetByIdAsync(Guid id);
+
+        void Create(ItemDto item);
+
+        void Update(ItemDto item);
+
+        void SoftDelete(ItemDto item);
+
+        void RecoverSoftDeleted(ItemDto item);
+    }
+}
