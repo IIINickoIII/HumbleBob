41737c846fcc033d35b9a8cbe47414dd646b37ac
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-08 22:50:18 +0200

    Added Dtos

---
+++ HumbleBob/HumbleBob.Bll/Dto/ItemDto.cs
@@ -0,0 +1,20 @@
+﻿using HumbleBob.Bll.Dto.Enums;
+using System;
+
+namespace HumbleBob.Bll.Dto
+{
+    public class ItemDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Desciption { get; set; }
+
+        public decimal Price { get; set; }
+
+        public StorageUnit StorageUnit { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ HumbleBob/HumbleBob.Bll/Mapper/MapperProfileBll.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HumbleBob.Bll.Dto;
+using HumbleBob.Dal.Entities;
+
+namespace HumbleBob.Bll.Mapper
+{
+    public class MapperProfileBll : Profile
+    {
+        public MapperProfileBll()
+        {
+            CreateMap<Item, ItemDto>().ReverseMap();
+        }
+    }
+}
