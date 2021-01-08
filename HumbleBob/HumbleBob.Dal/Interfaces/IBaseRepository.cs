63274c54a87ac6121adbb088b1868cc13afc22c1
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-08 19:33:20 +0200

    Added Repository and UoW interfaces

---
+++ HumbleBob/HumbleBob.Dal/Interfaces/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using HumbleBob.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HumbleBob.Dal.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        void Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void SoftDelete(TEntity entity);
+
+        void RecoverSoftDeleted(TEntity entity);
+    }
+}
