41915911772dbc8dd53aa97427f553a3fec09c71
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-11 10:03:31 +0200

    Added initial Web layer structure

---
+++ HumbleBob/HumbleBob.Web/ViewModels/ItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HumbleBob.Bll.Dto.Enums;
+using System;
+
+namespace HumbleBob.Web.ViewModels
+{
+    public class ItemViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Desciption { get; set; }
+
+        public decimal Price { get; set; }
+
+        public StorageUnit StorageUnit { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
