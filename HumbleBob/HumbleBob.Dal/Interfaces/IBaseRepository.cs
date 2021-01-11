fa398dec9a1d6bfe39472f985dc01ef75ae8d044
Mykola Andrushchenko <mykola.andrushchenko@gmail.com>
2021-01-11 17:52:26 +0200

    Extended repository functionality

--- HumbleBob/HumbleBob.Dal/Interfaces/IBaseRepository.cs
+++ HumbleBob/HumbleBob.Dal/Interfaces/IBaseRepository.cs
@@ -1,7 +1,10 @@
 ﻿using HumbleBob.Dal.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace HumbleBob.Dal.Interfaces
 {
@@ -15,8 +18,30 @@ namespace HumbleBob.Dal.Interfaces
 
         void Update(TEntity entity);
 
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        void HardDelete(TEntity entity);
+
+        void HardDeleteRange(IEnumerable<TEntity> entities);
+
         void SoftDelete(TEntity entity);
 
         void RecoverSoftDeleted(TEntity entity);
+
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+
+        Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+
+        IEnumerable<TEntity> GetAll<TSortField>(Expression<Func<TEntity, TSortField>> orderBy, bool @ascending);
+
+        Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
     }
 }
--- HumbleBob/HumbleBob.Dal/Repositories/BaseRepository.cs
+++ HumbleBob/HumbleBob.Dal/Repositories/BaseRepository.cs
@@ -1,9 +1,11 @@
-﻿using HumbleBob.Dal.Contexts;
-using HumbleBob.Dal.Entities;
+﻿using HumbleBob.Dal.Entities;
 using HumbleBob.Dal.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace HumbleBob.Dal.Repositories
@@ -11,10 +13,12 @@ namespace HumbleBob.Dal.Repositories
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
     {
         private readonly DbSet<TEntity> _dbSet;
+        private readonly IQueryable<TEntity> _queryableDbSet;
 
-        public BaseRepository(HumbleBobContext context)
+        public BaseRepository(DbContext context)
         {
             _dbSet = context.Set<TEntity>();
+            _queryableDbSet = _dbSet;
         }
 
         public void Create(TEntity entity)
@@ -22,9 +26,24 @@ namespace HumbleBob.Dal.Repositories
             _dbSet.Add(entity);
         }
 
-        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        public void Update(TEntity entity)
         {
-            return await _dbSet.AsNoTracking().ToListAsync();
+            _dbSet.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.UpdateRange(entities);
+        }
+
+        public void HardDelete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void HardDeleteRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
         }
 
         public async Task<TEntity> GetByIdAsync(Guid id)
@@ -32,19 +51,54 @@ namespace HumbleBob.Dal.Repositories
             return await _dbSet.SingleOrDefaultAsync(t => t.Id == id);
         }
 
-        public void RecoverSoftDeleted(TEntity entity)
-        {
-            _dbSet.Find(entity.Id).IsDeleted = false;
-        }
-
         public void SoftDelete(TEntity entity)
         {
             _dbSet.Find(entity.Id).IsDeleted = true;
         }
 
-        public void Update(TEntity entity)
+        public void RecoverSoftDeleted(TEntity entity)
         {
-            _dbSet.Update(entity);
+            _dbSet.Find(entity.Id).IsDeleted = false;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include)
+        {
+            return include(_queryableDbSet).Where(predicate).AsNoTracking();
+        }
+
+        public async Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.AsNoTracking().SingleAsync(predicate);
+        }
+
+        public async Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include)
+        {
+            return await include(_queryableDbSet).AsNoTracking().SingleAsync(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public IEnumerable<TEntity> GetAll<TSortField>(Expression<Func<TEntity, TSortField>> orderBy, bool @ascending)
+        {
+            return ascending ? _dbSet.OrderBy(orderBy) : _dbSet.OrderByDescending(orderBy);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include)
+        {
+            return await include(_queryableDbSet).AsNoTracking().ToListAsync();
         }
     }
 }
